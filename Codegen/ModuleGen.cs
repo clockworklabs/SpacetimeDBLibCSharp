a22cbc7bbb95a16d64f992fbaf857c8fc4799e05
Ingvar Stepanyan <me@rreverser.com>
2023-05-02 15:30:13 +0100

    Add codegen to the same repo

---
+++ Codegen/ModuleGen.cs
@@ -0,0 +1,188 @@
+namespace SpacetimeDB;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+using static Utils;
+
+[Generator]
+public class SpacetimeModuleGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var tables = context.SyntaxProvider.ForAttributeWithMetadataName(
+            fullyQualifiedMetadataName: "SpacetimeDB.TableAttribute",
+            predicate: (node, ct) => true, // already covered by attribute restrictions
+            transform: (context, ct) =>
+            {
+                var table = (TypeDeclarationSyntax)context.TargetNode;
+
+                var attrs = table.Members
+                    .OfType<FieldDeclarationSyntax>()
+                    .Where(f => !f.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword)))
+                    .SelectMany(f =>
+                    {
+                        var variantName =
+                            f.AttributeLists
+                                .SelectMany(a => a.Attributes)
+                                .Where(
+                                    a =>
+                                        context.SemanticModel.GetSymbolInfo(a).Symbol
+                                            is IMethodSymbol method
+                                        && method.ContainingType.ToDisplayString()
+                                            == "SpacetimeDB.ColumnIndexAttribute"
+                                )
+                                .Select(
+                                    a =>
+                                        a.ArgumentList?.Arguments[0].Expression switch
+                                        {
+                                            MemberAccessExpressionSyntax m
+                                                // Assume object part is already checked by compiler anyway.
+                                                => m.Name.Identifier.Text,
+                                            // In case someone did `using static SpacetimeDB.Module.ColumnIndexAttributeKind;`
+                                            IdentifierNameSyntax i => i.Identifier.Text,
+                                            null => null,
+                                            var expr
+                                                => throw new System.Exception(
+                                                    $"Unexpected expression {expr}"
+                                                )
+                                        }
+                                )
+                                .SingleOrDefault() ?? "UnSet";
+
+                        var variant = $"SpacetimeDB.Module.ColumnIndexAttributeKind.{variantName}";
+
+                        return Enumerable.Repeat(variant, f.Declaration.Variables.Count);
+                    })
+                    .ToArray();
+
+                return new
+                {
+                    Scope = new Scope(table),
+                    Name = table.Identifier.Text,
+                    FullName = SymbolToName(context.SemanticModel.GetDeclaredSymbol(table)!)
+                        .ToString(),
+                    Attrs = attrs
+                };
+            }
+        );
+
+        tables
+            .Select(
+                (t, ct) =>
+                {
+                    var extensions = t.Scope.GenerateExtensions(
+                        $@"
+                            private static uint tableId = SpacetimeDB.Bindings.GetTableId(nameof({t.Name}));
+
+                            public static IEnumerable<{t.Name}> Iter() =>
+                                new SpacetimeDB.Bindings.RawTableIter(tableId)
+                                .Select(GetSatsTypeInfo().ReadBytes);
+
+                            public void Insert() => SpacetimeDB.Bindings.Insert(
+                                tableId,
+                                GetSatsTypeInfo().ToBytes(this)
+                            );
+                        "
+                    );
+
+                    return new KeyValuePair<string, string>(t.FullName, extensions);
+                }
+            )
+            .RegisterSourceOutputs(context);
+
+        var addTables = tables
+            .Select(
+                (t, ct) =>
+                    $@"
+                module.Add(new SpacetimeDB.Module.TableDef(
+                    nameof({t.FullName}),
+                    module.AddType({t.FullName}.GetSatsTypeInfo().algebraicType)
+                    {string.Join("", t.Attrs.Select(a => $",\n{a}"))}
+                ));
+            "
+            )
+            .Collect();
+
+        var reducers = context.SyntaxProvider
+            .ForAttributeWithMetadataName(
+                fullyQualifiedMetadataName: "SpacetimeDB.ReducerAttribute",
+                predicate: (node, ct) => true, // already covered by attribute restrictions
+                transform: (context, ct) =>
+                {
+                    var method = (IMethodSymbol)
+                        context.SemanticModel.GetDeclaredSymbol(context.TargetNode)!;
+
+                    if (!method.ReturnsVoid)
+                    {
+                        throw new System.Exception($"Reducer {method} must return void");
+                    }
+
+                    return new
+                    {
+                        Name = method.Name,
+                        FullName = SymbolToName(method).ToString(),
+                        Args = method.Parameters.Select(p => (p.Name, p.Type)).ToArray(),
+                    };
+                }
+            )
+            .Select(
+                (r, ct) =>
+                    (
+                        r.Name,
+                        Class: $@"
+                            class {r.Name}: IReducer {{
+                                {string.Join("\n", r.Args.Select(a => $"SpacetimeDB.SATS.TypeInfo<{a.Type}> {a.Name} = {GetTypeInfo(a.Type)};"))}
+
+                                SpacetimeDB.Module.ReducerDef IReducer.MakeReducerDef() {{
+                                    return new (
+                                        nameof({r.FullName})
+                                        {string.Join("", r.Args.Select(a => $",\nnew SpacetimeDB.SATS.ProductTypeElement(nameof({a.Name}), {a.Name}.algebraicType)"))}
+                                    );
+                                }}
+
+                                void IReducer.Invoke(BinaryReader reader) {{
+                                    {r.FullName}({string.Join(", ", r.Args.Select(a => $"{a.Name}.read(reader)"))});
+                                }}
+                            }}
+                        "
+                    )
+            )
+            .Collect();
+
+        context.RegisterSourceOutput(
+            addTables.Combine(reducers),
+            (context, tuple) =>
+            {
+                var addTables = tuple.Left;
+                var reducers = tuple.Right;
+                if (addTables.IsEmpty && reducers.IsEmpty)
+                    return;
+                context.AddSource(
+                    "FFI.cs",
+                    $@"
+            using SpacetimeDB.Module;
+            using System.Runtime.CompilerServices;
+            using static SpacetimeDB.Bindings;
+
+            static class ModuleRegistration {{
+                {string.Join("\n", reducers.Select(r => r.Class))}
+
+#pragma warning disable CA2255
+                [ModuleInitializer]
+                public static void Register() {{
+                    {string.Join("\n", reducers.Select(r => $"FFI.RegisterReducer(new {r.Name}());"))}
+
+                    var module = FFI.Module;
+                    {string.Join("\n", addTables)}
+                }}
+#pragma warning restore CA2255
+            }}
+            "
+                );
+            }
+        );
+    }
+}
---
+++ Codegen/TypeGen.cs
@@ -0,0 +1,219 @@
+namespace SpacetimeDB;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using static Utils;
+
+struct VariableDeclaration
+{
+    public string Name;
+    public ITypeSymbol TypeSymbol;
+}
+
+[Generator]
+public class SpacetimeTypeGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.SyntaxProvider
+            .ForAttributeWithMetadataName(
+                fullyQualifiedMetadataName: "SpacetimeDB.TypeAttribute",
+                predicate: (node, ct) => true, // already covered by attribute restrictions
+                transform: (context, ct) =>
+                {
+                    var type = (TypeDeclarationSyntax)context.TargetNode;
+
+                    // Check if type implements generic `SpacetimeDB.TaggedEnum<Variants>` and, if so, extract the `Variants` type.
+                    var taggedEnumVariants = type.BaseList?.Types
+                        .OfType<SimpleBaseTypeSyntax>()
+                        .Select(t => context.SemanticModel.GetTypeInfo(t.Type, ct).Type)
+                        .OfType<INamedTypeSymbol>()
+                        .Where(
+                            t =>
+                                t.OriginalDefinition.ToString()
+                                == "SpacetimeDB.TaggedEnum<Variants>"
+                        )
+                        .Select(
+                            t =>
+                                (ImmutableArray<IFieldSymbol>?)
+                                    ((INamedTypeSymbol)t.TypeArguments[0]).TupleElements
+                        )
+                        .FirstOrDefault();
+
+                    var fields = type.Members
+                        .OfType<FieldDeclarationSyntax>()
+                        .Where(f => !f.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword)))
+                        .SelectMany(f =>
+                        {
+                            var typeSymbol = context.SemanticModel
+                                .GetTypeInfo(f.Declaration.Type, ct)
+                                .Type!;
+                            // Seems like a bug in Roslyn - nullability annotation is not set on the top type.
+                            // Set it manually for now. TODO: report upstream.
+                            if (f.Declaration.Type is NullableTypeSyntax)
+                            {
+                                typeSymbol = typeSymbol.WithNullableAnnotation(
+                                    NullableAnnotation.Annotated
+                                );
+                            }
+                            return f.Declaration.Variables.Select(
+                                v =>
+                                    new VariableDeclaration
+                                    {
+                                        Name = v.Identifier.Text,
+                                        TypeSymbol = typeSymbol,
+                                    }
+                            );
+                        });
+
+                    if (taggedEnumVariants is not null)
+                    {
+                        if (fields.Any())
+                        {
+                            throw new InvalidOperationException("Tagged enums cannot have fields.");
+                        }
+                        fields = taggedEnumVariants.Value
+                            .Select(
+                                v => new VariableDeclaration { Name = v.Name, TypeSymbol = v.Type, }
+                            )
+                            .ToArray();
+                    }
+
+                    return new
+                    {
+                        Scope = new Scope(type),
+                        FullName = SymbolToName(context.SemanticModel.GetDeclaredSymbol(type, ct)!)
+                            .ToString(),
+                        GenericName = $"{type.Identifier}{type.TypeParameterList}",
+                        IsTaggedEnum = taggedEnumVariants is not null,
+                        TypeParams = type.TypeParameterList?.Parameters
+                            .Select(p => p.Identifier.Text)
+                            .ToArray() ?? new string[] { },
+                        Members = fields,
+                    };
+                }
+            )
+            .Select(
+                (type, ct) =>
+                {
+                    var typeKind = type.IsTaggedEnum ? "Sum" : "Product";
+
+                    string read,
+                        write;
+
+                    var typeDesc = "";
+
+                    var fieldIO = type.Members.Select(
+                        m =>
+                            new
+                            {
+                                m.Name,
+                                Read = $"{m.Name} = fieldTypeInfo.{m.Name}.read(reader),",
+                                Write = $"fieldTypeInfo.{m.Name}.write(writer, value.{m.Name});"
+                            }
+                    );
+
+                    if (type.IsTaggedEnum)
+                    {
+                        typeDesc +=
+                            $@"
+                            public enum TagKind: byte
+                            {{
+                                {string.Join(", ", type.Members.Select(m => m.Name))}
+                            }}
+
+                            public TagKind Tag {{ get; private set; }}
+                            private object? boxedValue;
+                        ";
+
+                        typeDesc += string.Join(
+                            "\n",
+                            type.Members.Select(m =>
+                            {
+                                var name = m.Name;
+                                var type = m.TypeSymbol.ToDisplayString();
+
+                                return $@"
+                                public bool Is{name} => Tag == TagKind.{name};
+
+                                public {type} {name} {{
+                                    get => Is{name} ? ({type})boxedValue! : throw new System.InvalidOperationException($""Expected {name} but got {{Tag}}"");
+                                    set {{
+                                        Tag = TagKind.{name};
+                                        boxedValue = value;
+                                    }}
+                                }}
+                                ";
+                            })
+                        );
+
+                        read =
+                            $@"(TagKind)reader.ReadByte() switch {{
+                                {string.Join("\n", fieldIO.Select(m => $"TagKind.{m.Name} => new {type.GenericName} {{ {m.Read} }},"))}
+                                var tag => throw new System.InvalidOperationException($""Unknown tag {{tag}}"")
+                            }}";
+
+                        write =
+                            $@"writer.Write((byte)value.Tag);
+                            switch (value.Tag) {{
+                                {string.Join("\n", fieldIO.Select(m => $@"
+                                    case TagKind.{m.Name}:
+                                        {m.Write};
+                                        break;
+                                "))}
+                                default:
+                                    throw new System.InvalidOperationException($""Tagged enum is corrupted and has an unsupported tag {{value.Tag}}"");
+                            }}
+                        ";
+                    }
+                    else
+                    {
+                        read =
+                            $@"new {type.GenericName} {{
+                                {string.Join("\n", fieldIO.Select(m => m.Read))}
+                            }}";
+
+                        write = string.Join("\n", fieldIO.Select(m => m.Write));
+                    }
+
+                    typeDesc +=
+                        $@"
+// Workaround for C# not allowing multiple static constructors.
+
+public static SpacetimeDB.SATS.TypeInfo<{type.GenericName}> GetSatsTypeInfo({
+    string.Join(", ", type.TypeParams.Select(p => $"SpacetimeDB.SATS.TypeInfo<{p}> {p}TypeInfo"))
+}) {{
+    return SpacetimeDB.SATS.Typespace.RegisterType<{type.GenericName}>(() => {{
+        var fieldTypeInfo = new {{
+            {string.Join("\n", type.Members.Select(m => $"{m.Name} = {GetTypeInfo(m.TypeSymbol)},"))}
+        }};
+
+        return new SpacetimeDB.SATS.TypeInfo<{type.GenericName}>(
+            new SpacetimeDB.SATS.{typeKind}Type {{
+                {string.Join("\n", type.Members.Select(m => $"{{ nameof({m.Name}), fieldTypeInfo.{m.Name}.algebraicType }},"))}
+            }},
+            (reader) => {read},
+            (writer, value) => {{
+                {write}
+            }}
+        );
+    }});
+}}
+                    ";
+
+                    return new KeyValuePair<string, string>(
+                        type.FullName,
+                        type.Scope.GenerateExtensions(typeDesc)
+                    );
+                }
+            )
+            .RegisterSourceOutputs(context);
+    }
+}
